6cc53eb1007eb0986ad702a7dd2858b068347564
Wragnardev <shalithamza@gmail.com>
2022-12-01 22:14:30 +0300

    adding email system to program

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+#firebase modules for database
 import firebase_admin
 from firebase_admin import credentials, firestore
+
+#hashlib module for sha-256 encoding
 import hashlib
+
+#smtplib module for sending email
+import smtplib
+
+#random module for creating random number
+import random
 
 #firebase connection
 db = credentials.Certificate("./sign-up-sign-in-python-firebase-adminsdk-8kbfq-e0afe42a03.json")
 app = firebase_admin.initialize_app(db)
 db = firestore.client()
 
-#creating collection
+#creating a collection in firebase firestore database
 my_document = db.collection("users")
+
+#logging into email
+sender_email = "halitsbot"
+email_password = "rbflhssxgdwsbgkg"
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(sender_email, email_password)
 
 #login function
 def login():
@@ -28,7 +44,7 @@ def login():
             print("Account not found")
         else: 
             if data["password"] == hashed_my_password:
-                print("Giriş başarılı")
+                print("Login succesful")
     else:
         print("Wrong character please try again.")
 
@@ -38,19 +54,39 @@ def register():
     assert not my_document.document(new_name).get().exists, "This username is already taken, please choose another one."
     new_password = str(input("Password :\t"))
     hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-    my_document.document(new_name).set({
-        "password": hashed_password
-    })
+    new_email = str(input("E-mail :\t"))
+    num = str(random.randint(100000, 1000000))
+    server.sendmail(sender_email, new_email, num)
+    print("Email has been sent to ", new_email)
+    code = str(input("Write the code that has been sent to your email adress\t"))
+    if code == num:
+        my_document.document(new_name).set({
+            "email": new_email,
+            "password": hashed_password
+        })
+        print("Register succesful")
+    else:
+        print("Wrong code, please try again and check if your email is right.")
 
 #change password function
 def change_password():
     my_name = str(input("Name :\t"))
-    assert my_document.document(my_name).get().exits, "Wrong username, please try again"
-    new_password = str(input("Password :\t"))
-    hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-    my_document.document(my_name).set({
+    assert my_document.document(my_name).get().exists, "Wrong username, please try again"
+    my_email = str(input("E-mail :\t"))
+    num = str(random.randint(100000, 1000000))
+    server.sendmail(sender_email, my_email, num)
+    print("Email has been sent to ", my_email)
+    code = str(input("Write the code that has been sent to your email adress"))
+    if code == num:
+        new_password = str(input("Password :\t"))
+        hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
+        my_document.document(my_name).set({
+        "email": my_email,
         "password": hashed_password
-    })
+        })
+        print("Password changed succesfuly")
+    else:
+        print("Wrong code, please try again and check if your email is right.")
 
 #main function
 def main():
