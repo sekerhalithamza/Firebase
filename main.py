b67515b1bfe1a2fc59ab62ac1e6544957bdf86ee
Halit Hamza Şeker <shalithamza@gmail.com>
2023-06-21 16:18:22 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,348 +1,170 @@
-#firebase modules for database
+# Modules
+import hashlib
+import random
+import smtplib
+import ssl
+from email.message import EmailMessage
+
 import firebase_admin
 from firebase_admin import credentials, firestore
 
-#hashlib module for sha-256 encoding
-import hashlib
+# Connection to firebase
+cred = credentials.Certificate("firebase.json")
+firebase_admin.initialize_app(cred)
+cred = firestore.client()
 
-#smtplib module for sending email
-import smtplib
+# Creating collections
+users = cred.collection("users")
+admins = cred.collection("admins")
 
-#random module for creating random number
-import random
-
-#firebase connection
-db = credentials.Certificate("firebase.json")
-firebase_admin.initialize_app(db)
-db = firestore.client()
-
-#creating a collection in firebase firestore database
-user_document = db.collection("users")
-admin_document = db.collection("admin")
-
-#logging into email
+# Email bot
 sender_email = "halitsbot@gmail.com"
-email_password = "rbflhssxgdwsbgkg"
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.starttls()
-server.login(sender_email, email_password)
+email_password = open("password.txt", "r").read()
 
-#login function
-def login():
-    global my_name
-    global data 
-    my_name = str(input("Name :\t"))
-    assert user_document.document(my_name).get().exists, "Wrong username, please try again."
-    my_password = str(input("Password :\t"))
-    hashed_my_password = hashlib.sha256(my_password.encode()).hexdigest()
-    try:
-        data = user_document.document(my_name).get().to_dict()
-        data["password"]
-    except KeyError:
-        print('Account not found')
-    except AttributeError:
-        print("Account not found")
-    else: 
-        if data["password"] == hashed_my_password:
+
+class User:
+    def __init__(self) -> None:
+        pass
+
+    def user_panel(self):
+        print(
+            f"""
+            Welcome {user_name}
+            To change your password press 1
+            To change your email press 2
+            To change your username press 3
+            To logout press 4"""
+        )
+        user_string = input("Enter your choice:\t")
+        if user_string == "1":
+            return
+        elif user_string == "2":
+            return
+        elif user_string == "3":
+            return
+        elif user_string == "4":
+            return
+
+    def login(self):
+        global user_name
+        user_name = input("Enter your username:\t")
+        if users.document(user_name).get().exists == False:
+            print("User could not found")
+            user_name = None
+            self.register()
+        # try:
+        #     data = users.document(user_name).get().to_dict()
+        #     data["email"]
+        #     data["password"]
+        # except KeyError:
+        #     print("User not found")
+        #     # main()
+        # except AttributeError:
+        #     print("User not found")
+        #     # main()
+        # else:
+        data = users.document(user_name).get().to_dict()
+        user_email = input("Enter your email:\t")
+        if user_email == data["email"]:
+            user_password = input("Enter your password:\t")
+            user_password = hashlib.sha256(user_password.encode()).hexdigest()
+            if user_password == data["password"]:
+                print("Login successful")
+                users.document(user_name).update({"status": "online"})
+                self.user_panel()
+            else:
+                print("Wrong password")
+                self.login()
+        else:
+            print("Wrong email")
+            self.login()
+
+    def register(self):
+        new_name = input("Enter your username:\t")
+        if users.document(new_name).get().exists == True:
+            print("Username already taken")
+            self.register()
+        new_email = input("Enter your email:\t")
+        code = random.randint(100000, 999999)
+        email_verification(new_name, new_email, code)
+        user_code = input("Enter the code we sent to your email:\t")
+        if user_code == code:
+            new_password = input("Enter your password:\t")
+            new_password = hashlib.sha256(new_password.encode()).hexdigest()
+            users.document(new_name).set(
+                {"email": new_email, "password": new_password, "status": "offline"}
+            )
+            print("Registration successful")
+            self.login()
+
+    @staticmethod
+    def logout():
+        users.document(user_name).update({"status": "offline"})
+        print("Logout successful")
+
+
+class Admin:
+    def admin_panel(self):
+        return
+
+    def login(self):
+        global admin_name
+        admin_name = str(input("Enter your username:\t"))
+        if admins.document(admin_name).get().exists == False:
+            print("Admin coul not found")
+        admin_email = str(input("Enter your email:\t"))
+        data = admins.document(admin_name).get().to_dict()
+        if admin_email == data["email"]:
+            admin_password = str(input("Enter your password:\t"))
+            admin_password = hashlib.sha256(admin_password.encode()).hexdigest()
+            if admin_password == data["password"]:
                 print("Login succesful")
-                user_document.document(my_name).set({
-                    "status": "online",
-                    "email": data["email"],
-                    "password": data["password"]
-                })
-                user_console()
-        else:
-            print("Wrong password please try again.")
+                self.admin_panel()
 
-#log out function
-def log_out():
-    print("Logging out")
-    user_document.document(my_name).set({
-        "status": "offline",
-        "email": data["email"],
-        "password": data["password"]
-    })
 
-#register function
-def register():
-    new_name = str(input("Name :\t"))
-    assert not user_document.document(new_name).get().exists, "This username is already taken, please choose another one."
-    new_password = str(input("Password :\t"))
-    hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-    new_email = str(input("E-mail :\t"))
-    num = str(random.randint(100000, 1000000))
-    server.sendmail(sender_email, new_email, num)
-    print("Email has been sent to ", new_email)
-    code = str(input("Write the code that has been sent to your email adress\t"))
-    if code == num:
-        user_document.document(new_name).set({
-            "email": new_email,
-            "password": hashed_password
-        })
-        print("Register succesful")
-    else:
-        print("Wrong code, please try again and check if your email is right.")
+# variables of current users
+current_user = User()
+current_admin = Admin()
 
-#change password function
-def change_password():
-    my_email = str(input("E-mail :\t"))
-    try:
-        data = user_document.document(my_name).get().to_dict()
-        data["email"]
-    except KeyError:
-        print("Email not found")
-    except AttributeError:
-        print("Email not found")
-    else:
-        if data["email"] == my_email:
-            num = str(random.randint(100000, 1000000))
-            server.sendmail(sender_email, my_email, num)
-            print("Email has been sent to ", my_email)
-            code = str(input("Write the code that has been sent to your email adress"))
-            if code == num:
-                new_password = str(input("New password :\t"))
-                hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-                user_document.document(my_name).set({
-                    "email": my_email,
-                    "password": hashed_password,
-                    "status": "online"
-                })
-                print("Password changed succesfuly")
-                user_console()
-            else:
-                print("Wrong code, please try again and check if your email is right.")
-        else:
-            print("Wrong Email")
 
-def user_console():
-    print("To change password press c or C,\nTo log out press b or B")
-    while True:
-        user_string = str(input("")).upper()
-        match user_string:
-            case "C":
-                print("Directing")
-                change_password()
-            case "B":
-                log_out()
-                break
+# Email verification
+def email_verification(user_name, user_email, code):
+    body = f"""
+    Hello {user_name},
+    We see that you are trying to use our service.
+    
+    For this you need to verify yourself. You can verify yourself with the code below.
+    2 Factor Authentication Code: {code}
+    
+    If you are not trying to use our service, please ignore this email.
+    Thanks for you'r attention."""
 
-#admin console function
-def admin_login():
-    my_name = str(input("Name :\t"))
-    assert admin_document.document(my_name).get().exists, "Wrong username, please try again"
-    my_email = str(input("E-mail :\t"))
-    try:
-        data = admin_document.document(my_name).get().to_dict()
-    except KeyError:
-        print("Email not found")
-    except AttributeError:
-        print("Email not found")
-    else:
-        my_password = str(input("Pasword :\t"))
-        hashed_password = hashlib.sha256(my_password.encode()).hexdigest()
-        if data["password"] == hashed_password:
-            if data["email"] == my_email:
-                num = str(random.randint(100000, 1000000))
-                server.sendmail(sender_email, my_email, num)
-                print("Email has been sent to ", my_email)
-                code = str(input("Write the code that has been sent to your email adress"))
-                if code == num:
-                    print("Login succesful")
-                    admin_console()
-                else:
-                    print("Wrong code, please try again and check if your email is right.")
-            else:
-                print("Wrong email")
-        else:
-            print("Wrong password")
+    mail = EmailMessage()
+    mail["Subject"] = "Email verification"
+    mail["From"] = sender_email
+    mail["To"] = user_email
+    mail.set_content(body)
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, email_password)
+        server.sendmail(sender_email, user_email, mail.as_string())
 
-#adding a new admin to firebase database function
-def add_admin():
-    new_name = str(input("Name :\t"))
-    assert not admin_document.document(new_name).get().exists, "This username is already taken, please choose another one."
-    new_password = str(input("Password :\t"))
-    hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-    new_email = str(input("E-mail :\t"))
-    num = str(random.randint(100000, 1000000))
-    server.sendmail(sender_email, new_email, num)
-    print("Email has been sent to ", new_email)
-    code = str(input("Write the code that has been sent to your email adress\t"))
-    if code == num:
-        admin_document.document(new_name).set({
-            "email": new_email,
-            "password": hashed_password
-        })
-        print("Register succesful")
-    else:
-        print("Wrong code, please try again and check if your email is right.")
 
-#adding a new user to firebase database function
-def add_user():
-    new_name = str(input("Name :\t"))
-    assert not user_document.document(new_name).get().exists, "This username is already taken, please choose another one."
-    new_password = str(input("Password :\t"))
-    hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-    new_email = str(input("E-mail :\t"))
-    num = str(random.randint(100000, 1000000))
-    server.sendmail(sender_email, new_email, num)
-    print("Email has been sent to ", new_email)
-    code = str(input("Write the code that has been sent to your email adress\t"))
-    if code == num:
-        user_document.document(new_name).set({
-            "email": new_email,
-            "password": hashed_password
-        })
-        print("Register succesful")
-    else:
-        print("Wrong code, please try again and check if your email is right.")
-
-#deleting a user from firebase database function
-def delete_user():
-    my_name = str(input("Name :\t"))
-    assert user_document.document(my_name).get().exists, "There is not such an user name, please try again"
-    user_document.document(my_name).delete()
-    print("User deleted succesfuly")
-
-#deleting an admin from firebase database function
-def delete_admin():
-    my_name = str(input("Name :\t"))
-    assert admin_document.document(my_name).get().exists, "There is not such an admin name, please try again"
-    admin_document.document(my_name).delete()
-    print("Admin deleted succesfuly")
-
-#changing an admin's credentials from firebase database function
-def change_admin_credentials():
-    cred = str(input("""Do you want to change email or password
-For email press e,
-For password press p,
-"""))
-    match cred:
-        case "e":
-            print("Directing")
-            change_admin_email()
-        case "p":
-            print("Directing")
-            change_admin_password()
-
-#changing an user's credentials from firebase database function
-def change_user_credentials():
-    cred = str(input("""Do you want to change email or password
-For email press e,
-For password press p,
-"""))
-    match cred:
-        case "e":
-            print("Directing")
-            change_user_email()
-        case "p":
-            print("Directing")
-            change_user_password()
-
-#changing an admin's email from firebase database function
-def change_admin_email():
-    my_name = str(input("Name :\t"))
-    assert admin_document.document(my_name).get().exists, "There is not such an admin name, please try again"
-    new_email = str(input("New email :\t"))
-    admin_document.document(my_name).set({
-        "email": new_email
-    })
-
-#changing an admin's password from firebase database function
-def change_admin_password():
-    my_name = str(input("Name :\t"))
-    new_data = admin_document.document(my_name).get().to_dict()
-    assert admin_document.document(my_name).get().exists, "There is not such an admin name, please try again"
-    new_password = str(input("New password :\t"))
-    hashed_password = hashlib.sha256(new_password.encode()).hexdigest()
-    admin_document.document(my_name).set({
-        "password": hashed_password,
-    })
-
-#changing an user's email from firebase database functiın
-def change_user_email():
-    my_name = str(input("Name :\t"))
-    assert user_document.document(my_name).get().exists, "There is not such an user name, please try again"
-    new_email = str(input("New email :\t"))
-    user_document.document(my_name).set({
-        "email": new_email
-    })
-
-#changing an user's password from firebase database function
-def change_user_password():
-    my_name = str(input("Name :\t"))
-    assert user_document.document(my_name).get().exists, "There is not such an admin name, please try again"
-    new_password = str(input("New password :\t"))
-    user_document.document(my_name).set({
-        "password": new_password
-    })
-
-#admin console function
-def admin_console():
-    while True:
-        print("""
-To add new admin press 1,
-
-To delete an admin press 2,
-
-To change an admins password or email press 3,
-
-To add a new user press 4,
-
-To delete an user press 5,
-
-To change an users password or email press 6,
-
-To log out press 7.
-""")
-        admin_string = str(input(""))
-        match admin_string:
-            case "1":
-                print("Directing...")
-                add_admin()
-            case "2":
-                print("Directing...")
-                delete_admin()
-            case "3":
-                print("Directing...")
-                change_admin_credentials()
-            case "4":
-                print("Directing...")
-                add_user()
-            case "5":
-                print("Directing...")
-                delete_user()
-            case "6":
-                print("Directing...")
-                change_user_credentials()
-            case "7":
-                print("Logging out")
-                break
-
-#main function
-def main():
-    while True:
-        print("""
-To login press l or L,
-
-To register press r or R,
-
-To exit press b or B.
-""")
-        user_string = str(input("")).upper()
-        match user_string:
-            case "L":
-                login()
-            case "R":
-                register()
-            case "1929":
-                print("You reached to admin panel.")
-                print("Please log in.")
-                admin_login()
-                break
-            case "B":
-                print("Exit...")
-                break
-
-#program starts
 if __name__ == "__main__":
-    main()
+    print("Welcome to our system")
+    print("If you want to go to user console please press 'u'")
+    user_string = str(input()).upper()
+    if user_string == "U":
+        print("If you want to register press 'r'")
+        print("If you want to login press 'l'")
+        user_string = str(input()).upper()
+        if user_string == "L":
+            current_user.login()
+        elif user_string == "R":
+            current_user.register()
+        else:
+            print("Please enter a valid value")
+    elif user_string == "A":
+        current_admin.login()
+    else:
+        print("Please enter a valid value")
